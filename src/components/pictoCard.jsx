7f9b399f3f6c2a3a9f02a5c4bc07c61aa7b0690e
Arjun <arjundubey2906@gmail.com>
2026-03-07 20:14:15 +0530

    changes to card styling and background

--- src/components/pictoCard.jsx
+++ src/components/pictoCard.jsx
@@ -4,7 +4,7 @@ import img from '../assets/pictoimg.png'
 function PictoCard({ name, found}) {
   const bg = found === 'FOUND!' ? '#28cb6c' : '#1a1a1a'
   return (
-    <div className="card" style={{ background: '#252525', borderColor: bg, boxShadow: `0px 1px 5px ${bg}` }}>
+    <div className="card" style={{ backgroundColor: '#252525', borderColor: bg, boxShadow: `0px 1px 5px ${bg}` }}>
       <img src={img} className="pictoimg" alt="picto" style={{marginTop:'15px'}} />
       <hr style={{ width: '60%', margin: '8px auto', borderColor: '#a2b8dd', boxShadow: '0px 1px 6px rgba(0,0,0,0.6)' }} />
       <h3 style={{ color: '#dcc6a2' }}>{name}</h3>
